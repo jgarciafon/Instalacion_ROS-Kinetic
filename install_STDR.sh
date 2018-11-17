1292ca3784b6e20e0d87b0bd03b7fd41b8d2eea8
Javier GF <45110776+jgarciafon@users.noreply.github.com>
2018-11-17 15:21:04 +0100

    Create install_STDR.sh

---
+++ install_STDR.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+####################################################################################
+###### STDR + aux_files (error odometria) + mapa eurobot19 + robot eurobot19 #######
+####################################################################################
+
+# Instalar el simulador
+cd ~/Instalacion_ROS-Kinetic
+mv stdr_simulator-indigo-devel.zip ~/robotica_movil_ws/src
+unzip stdr_simulator-indigo-devel.zip
+rm stdr_simulator-indigo-devel.zip
+cd ..
+rospack profile
+catkin_make
+
+# Instalar el error de odometria
+cd ~/Instalacion_ROS-Kinetic
+mv aux_files.zip ~/robotica_movil_ws/src
+unzip aux_files.zip
+rm aux_files.zip
+cd ..
+catkin_make
