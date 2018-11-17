0366d124a4b0ac7c124ad72e57fdcee75b485986
Javier GF <45110776+jgarciafon@users.noreply.github.com>
2018-11-17 14:53:00 +0100

    Update install_and_configuring_ROS_environment.sh

--- install_and_configuring_ROS_environment.sh
+++ install_and_configuring_ROS_environment.sh
@@ -4,8 +4,6 @@
 #################### Installing and Configuring Your ROS Environment ######################
 ###########################################################################################
 
-mkdir -p ~/robotica_movil_ws/src
-
 cd ~/robotica_movil_ws/src/
 catkin_init_workspace
 cd ~/robotica_movil_ws
@@ -15,6 +13,6 @@ echo "source ~/robotica_movil_ws/devel/setup.bash" >> ~/.bashrc
 echo "export ROS_WORKSPACE=~/robotica_movil_ws/" >> ~/.bashrc
 echo "export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WORKSPACE" >> ~/.bashrc
 echo "export ROSCONSOLE_FORMAT='[${severity}] [${time}]: ${message}'" >> ~/.bashrc
-echo "export ROS_MASTER_URI=http://192.168.1.1" >> ~/.bashrc
+echo "export ROS_MASTER_URI=http://192.168.1.1:11311" >> ~/.bashrc
 echo "export ROS_IP=192.168.1.1" >> ~/.bashrc
 source ~/.bashrc
