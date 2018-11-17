08466fd512da1e8d1b371d9fe42013aa9b0f21d6
Javier GF <45110776+jgarciafon@users.noreply.github.com>
2018-11-17 14:39:21 +0100

    Update install_and_configuring_ROS_environment.sh

--- install_and_configuring_ROS_environment.sh
+++ install_and_configuring_ROS_environment.sh
@@ -4,3 +4,14 @@
 #################### Installing and Configuring Your ROS Environment ######################
 ###########################################################################################
 
+mkdir –p ~/robotica_movil_ws/src
+
+cd ~/robotica_movil_ws/src/
+catkin_init_workspace
+cd ~/robotica_movil_ws
+catkin_make
+
+echo "\n# ROS Kinetic " >> ~/.bashrc
+echo "source ~/robotica_movil_ws/devel/setup.bash" >> ~/.bashrc
+echo "export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/robotica_movil_ws/" >> ~/.bashrc
+source ~/.bashrc
