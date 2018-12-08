31be8f55b4d88452940cc002d05ff715c1809033
Javier GF <45110776+jgarciafon@users.noreply.github.com>
2018-12-08 23:03:48 +0100

    Update install_STDR.sh

--- install_STDR.sh
+++ install_STDR.sh
@@ -6,8 +6,8 @@
 
 # Instalar el simulador
 cd ~/Instalacion_ROS-Kinetic
-mv stdr_simulator-indigo-devel.zip ~/robotica_movil_ws/src
-cd ~/robotica_movil_ws/src
+mv stdr_simulator-indigo-devel.zip ~/eurobot_ws/src
+cd ~/eurobot_ws/src
 unzip stdr_simulator-indigo-devel.zip
 rm stdr_simulator-indigo-devel.zip
 cd ..
@@ -16,8 +16,8 @@ catkin_make
 
 # Instalar el error de odometria
 cd ~/Instalacion_ROS-Kinetic
-mv aux_files.zip ~/robotica_movil_ws/src
-cd ~/robotica_movil_ws/src
+mv aux_files.zip ~/eurobot_ws/src
+cd ~/eurobot_ws/src
 unzip aux_files.zip
 rm aux_files.zip
 cd ..
